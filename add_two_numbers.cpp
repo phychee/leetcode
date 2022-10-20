a5eb7f698ed30c8b54fa56f4861a311068e4b444
Marie Zhussupova <102762019+phychee@users.noreply.github.com>
2022-10-20 10:48:48 +0300

    Update add_two_numbers.cpp

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -1,3 +1,5 @@
+/* Add Two Numbers without creating new linked list */
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,45 +13,77 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
         int carry = 0;
+        int length1 = 0;
+        int length2 = 0;
+
+        ListNode* counter1 = l1;
+        ListNode* counter2 = l2;
+
         ListNode* temp1 = NULL;
         ListNode* temp2 = NULL;
-        ListNode* head;
-        head = l1;
 
+        ListNode* head1 = NULL;
+        ListNode* head;
+
+        while (counter1) {
+            length1++;
+            counter1 = counter1->next;
+        }
+
+        while (counter2) {
+            length2++;
+            counter2 = counter2->next;
+        }
+
+        if (length1 >= length2) {
+            head = l1;
+        }
+        else {
+            head = l2;
+        }
+
+        ListNode* result = head;
+        ListNode* head2 = head;
+        
         while (l1 && l2) {
-            l1->val = l1->val + l2->val + carry;
+            head->val = l1->val + l2->val + carry;
             carry = 0;
-            if (l1->val >= 10) {
-                l1->val = l1->val % 10;
+            if (head->val >= 10) {
+                head->val = head->val % 10;
                 carry++;
             }
+            head1 = l1;
+            head = head->next;
             l1 = l1->next;
             l2 = l2->next;
         }
 
         if (l1) {
             while (l1) {
-                l1->val = l1->val + carry;
+                head->val = l1->val + carry;
                 carry = 0;
-                if (l1->val >= 10) {
-                    l1->val = l1->val % 10;
+                if (head->val >= 10) {
+                    head->val = head->val % 10;
                     carry++;
                 }
                 temp1 = l1;
+                head = head->next;
                 l1 = l1->next;
             }
         }
 
         if (l2) {
-            while (l2->next) {
-                l2->val = l2->val + carry;
+            while (l2) {
+                head->val = l2->val + carry;
                 carry = 0;
-                if (l2->val >= 10) {
-                    l2->val = l2->val % 10;
+                if (head->val >= 10) {
+                    head->val = head->val % 10;
                     carry++;
                 }
                 temp2 = l2;
+                head = head->next;
                 l2 = l2->next;
             }
         }
@@ -58,8 +92,19 @@ public:
             ListNode* new_node;
             new_node = new ListNode();
             new_node->val = carry;
-            temp1->next = new_node;
+            if ((length1 == 1) && (length1 == length2)) {
+                head2->next = new_node;
+            }
+            else if (length1 == length2) {
+                head1->next = new_node;
+            }
+            else if (length1 >= length2){
+                temp1->next = new_node;
+            }
+            else if (length1 < length2){
+                temp2->next = new_node;
+            }
         }
-        return head;
+        return result;
     }
 };
